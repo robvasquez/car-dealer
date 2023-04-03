4cd9fd5e893aa420a27224ba1a23e8fc55cca74b
Rob Vasquez <rvasquez@18techs.com>
2023-04-02 20:24:03 -0600

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import '../styles/tailwind.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
+// The rest of the code remains unchanged
+
+function MyApp({Component, pageProps}: AppProps) {
   return <Component {...pageProps} />
 }
+
 export default MyApp
